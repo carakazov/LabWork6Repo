df87ee836242d0639034a6d0fa6a86f9e8956296
Vadim <carakazov@yandex.ru>
2020-12-17 02:47:16 +0300

    from first branch

--- GitHubLab/Program.cs
+++ GitHubLab/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace GitHubLab
+﻿using System;
+
+namespace GitHubLab
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello from first branch");
         }
     }
 }
